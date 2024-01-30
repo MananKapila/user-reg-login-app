88bc985888bd324eb9854ca25cd00517b92216a7
Manan <manankapiladps@gmail.com>
2024-01-30 20:48:17 +0530

    extending spring security for login impl

--- src/main/java/com/demo/project/user_registration_login/repository/UserRepository.java
+++ src/main/java/com/demo/project/user_registration_login/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.demo.project.user_registration_login.repository;
 
 import com.demo.project.user_registration_login.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("Select * from User u where u.email = ?1")
+    User findByEmail(String email);
 }
